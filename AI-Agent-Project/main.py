5f4a8751ac7e580b745543cdc8158b3da5ba80de
Leonardo Silva <leonardosilvapro@outlook.com>
2026-04-12 17:03:03 +0100

    Added token tracking, defined roles and improved categories.

--- AI-Agent-Project/main.py
+++ AI-Agent-Project/main.py
@@ -12,18 +12,38 @@ OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 if OLLAMA_URL == None or OLLAMA_MODEL == None:
     raise RuntimeError("No Ollama URL or Ollama model detected. Cannot proceed.")
 
-def generate(prompt):
+messages = []
+messages.append({"role": "system", "content":"..."})
+
+def generate(prompt, messages):
+
+    #user input
+    messages.append({"role": "user", "content": prompt})
+
     response = requests.post(OLLAMA_URL, json={
             "model": OLLAMA_MODEL,
-            "prompt": prompt,
+            "messages": messages,
             "stream": False
         }
     )
-    return response.json()
+    
+    data = response.json()
+
+    #extract assistant reply
+    reply = data["message"]["content"]
+
+    #add assistant reply to history
+    messages.append({"role": "assistant", "content": reply })
+
+    return data
+
+# def bootdev_api_format_converter(messages):
+    # role="user", parts=[{"text": "..."}]
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("prompt", nargs="*")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output.")
 
     args = parser.parse_args()
     user_input = " ".join(args.prompt)
@@ -31,10 +51,14 @@ def main():
     if not user_input.strip():
         parser.error("Prompt cannot be empty.")
 
-    response = generate(user_input)
-    print(response["response"])
-    print(f"Prompt tokens:{response["prompt_eval_count"]}")
-    print(f"Response tokens:{response["eval_count"]}")
+    response = generate(user_input, messages)
+
+    if args.verbose:
+        print(f'User prompt:{response["message"]["content"]}')
+        print(f'Prompt tokens:{response.get("prompt_eval_count")}')
+        print(f'Response tokens:{response.get("eval_count")}')
+    else:
+        print(response["message"]["content"])
 
 if __name__ == "__main__":
     main()
